08cf3016a8716fcccb678dd22531b2a98e092063
Richard Todd <Richardjamestodd@hotmail.co.uk>
2023-11-27 13:55:22 +0000

    Create Question1.ts

---
+++ Question1.ts
@@ -0,0 +1,14 @@
+/**  1. Smallest missing integer
+Write a function that will receive a single parameter, A, an array of integers. 
+Each element of the array is an integer in the range -100,000 to 100,000. 
+The maximum length of array A is 10,000. The function will return the smallest 
+positive integer which is not in array A.
+
+E.g. [1, 4, 6, 3, 2] returns 5. */
+
+function SmallestMissingInt(){
+
+
+}
+
+
